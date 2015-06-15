0736a13afae092b49b5a5cc6d34c0d46a69fb021
charlotterouby <charlotte.rouby@hotmail.fr>
2015-06-15 09:21:48 +0200

    Modifications vendredi 12/06

--- front-page.php
+++ front-page.php
@@ -1,10 +1,41 @@
 <!--appel de l'en-tête-->
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
 <!-- contenu principal-->
 <main id="front-page" class="grid-100 grid-parent">
-    <?php dynamic_sidebar('home'); ?>
+	<div id="slideshow" class="grid-100 grid-parent hide-on-mobile">
+		<!-- conteneur slider -->
+		<ul id="sContent">
+
+			<li class="homeSlide">
+				<img src="http://localhost/siarep/wp-content/uploads/fonds_img/slider-cannes-1920-640.jpg" alt="Chantier du palais des festivals de Cannes" />
+				<div class="bg"></div>
+				<h2 class="h1 grid-50">SIAREP 45 ans d'expérience de l'enveloppe du bâtiment</h2>
+
+			</li>
+
+			<li class="homeSlide">
+				<img src="http://localhost/siarep/wp-content/uploads/fonds_img/slider-grands-cypres.jpg" alt="Chantier de la Résidence des Grands Cyprès à Avignon" />
+				<div class="bg"></div>
+				<h2 class="h1 grid-50">Gagner en qualité architecturale</h2>
+			</li>
+
+			<li class="homeSlide">
+				<img src="http://localhost/siarep/wp-content/uploads/fonds_img/slider-jules-ferry-192-640.jpg" alt="Chantier du lycée Jules Ferry de Montpellier" />
+				<div class="bg"></div>
+				<h2 class="h1 grid-50">Une force régionale à votre service</h2>
+			</li>
+
+		</ul>
+	</div>
+
+	<div id="homeSlide4" class="gde-section-image grid-100 grid-parent hide-on-desktop">
+		<div class="homeSlide mobile-grid-100 grid-parent">
+			<div class="bgmobile mobile-grid-100 grid-paent"></div>
+			<h2 class="h1 mobile-grid-75">SIAREP spécialiste de l'enveloppe de bâtiment</h2>
+		</div>
+	</div>
 </main>
 
 <!-- appel du pied de page-->
-<?php get_footer(); ?> 
+<?php get_footer(); ?>
